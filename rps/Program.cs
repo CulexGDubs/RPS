7031e634b6cc56097797dd6cd1982cd2d43f55cd
culexgdubs <d4n13lb3rry@gmail.com>
2021-12-01 11:51:09 -0500

    rps

---
+++ rps/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace rps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string result = p.rps("scissors", "rock");
+            System.Console.WriteLine(result);
+        }
+
+        public string rps(string a, string b)
+        {
+            if(a == b)
+            {
+                return "TIE";
+            }
+            
+            switch (a)
+            {
+                case "rock": if(b == "paper"){
+                    return "Player 2 wins";
+                }
+                else{
+                    return "Player 1 wins";
+                }
+
+                case "paper": if(b == "scissors"){
+                    return "Player 2 wins";
+                }
+                else{
+                    return "Player 1 wins";
+                }
+
+                case "scissors": if(b == "rock"){
+                    return "Player 2 wins";
+                }
+                else{
+                    return "Player 1 wins";
+                }
+
+                default: return "invalid input";
+            }
+        }
+    }
+}
